d5b71fd958f04f21270a41b5ee76df6542719676
joaozinofodao-ctrl <joaozinofodao@gmail.com>
2026-03-05 22:41:05 -0300

    Add files via upload

---
+++ AeD1/1stLesson.py
@@ -0,0 +1,7 @@
+#type () diz o tipe ta ligado ??!??
+idade = int(input ("idade"))
+
+print("sua idade é" " " idade )
+print(idade + 2)
+
+#casting converte tipos
---
+++ AeD1/bogaGrandão.py
@@ -0,0 +1,10 @@
+print("wins?")
+W = int(input())
+print("loses?")
+L = int(input())
+
+games = W + L 
+
+WR = (W*100)/games
+print("winRate:" + str(WR))
+print("total gaimes" + str(games))
